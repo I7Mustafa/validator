453da6d30ed028238b276e9dffe6bf745c7125fb
Mustafa Ibrahim <Mustafa7Ibrahim@gmail.com>
2023-02-21 10:49:27 +0200

    add testing and optimize

--- lib/validator.dart
+++ lib/validator.dart
@@ -1,98 +1,112 @@
 library validator;
 
-import 'validate/custom_validate.dart';
-
 class Validator {
-  static String? passwordValidate(String? value) {
-    final passwordValidator = CustomValidate.validatePassword(value);
-    if (passwordValidator == null) {
+  static String? name(String? value) {
+    if (value == null) {
       return null;
-    } else {
-      return passwordValidator;
     }
-  }
-
-  static String? restrictedValidatePassword(String? value) {
-    final passwordValidator = CustomValidate.restrictedValidatePassword(value);
-    if (passwordValidator == null) {
-      return null;
-    } else {
-      return passwordValidator;
+    if (value.isEmpty) {
+      return 'Please Enter Name';
     }
-  }
 
-  static String? passwordConfirmValidate(String? value, String? confirmValue) {
-    final passwordValidator =
-        CustomValidate.validatePassword(value, confirmPassword: confirmValue);
-    if (passwordValidator == null) {
-      return null;
-    } else {
-      return passwordValidator;
-    }
-  }
-
-  static String? emailValidate(String? value) {
-    final emailValidator = CustomValidate.validateEmail(value);
-    if (emailValidator == null) {
-      return null;
-    } else {
-      return emailValidator;
-    }
-  }
-
-  static String? phoneValidate(String? value) {
-    final phoneValidator = CustomValidate.validatePhoneNumber(value);
-    if (phoneValidator == null) {
-      return null;
-    } else {
-      return phoneValidator;
-    }
-  }
-
-  static String? nameValidate(String? value) {
-    final nameValidator = CustomValidate.validateName(value);
-    if (nameValidator == null) {
-      return null;
-    } else {
-      return nameValidator;
-    }
-  }
-
-  static bool isValidFullName(String name) {
-    return name.contains(' ');
-  }
-
-  static String? creditValidate(String? value) {
-    final validator = CustomValidate.validateCreditCardNumber(value);
-    if (validator == null) {
-      return null;
-    } else {
-      return validator;
-    }
-  }
-
-  static String? cvvValidate(String? value) {
-    final validator = CustomValidate.validateCVV(value);
-    if (validator == null) {
-      return null;
-    } else {
-      return validator;
-    }
-  }
-
-  static String? isEmptyText(String? value) {
-    if (value!.isEmpty) {
-      return " ";
+    if (value.length < 3) {
+      return 'Name must be at least 3 characters';
     }
     return null;
   }
 
-  static String? dateValidate(String? value) {
-    final validator = CustomValidate.validateDate(value);
-    if (validator == null) {
+  static String? email(String? value, {String? customMessage}) {
+    if (value == null) return null;
+    if (value.isEmpty) {
+      return customMessage ?? "Please enter email";
+    }
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value)) return customMessage ?? 'Enter Valid Email';
+
+    return null;
+  }
+
+  static String? selectCountry(String? value) {
+    if (value == null) {
+      return null;
+    }
+    if (value.isEmpty) {
+      return 'Please select country';
+    }
+    return null;
+  }
+
+  static String? phoneNumber(String? value) {
+    String regexPattern = r'^(?:[+0][1-9])?[0-9]{10,12}$';
+    var regExp = RegExp(regexPattern);
+    if (value == null) {
+      return null;
+    }
+    if (value.isEmpty) {
+      return 'Please enter a phone number';
+    }
+    if (value.length <= 10) {
+      return 'Please enter with number minimum 10';
+    }
+    if (!value.startsWith("0")) {
+      return 'Please enter a valid phone number';
+    }
+    if (!regExp.hasMatch(value)) {
+      return 'Please enter a valid phone number';
+    }
+    return null;
+  }
+
+  static String? password(String? value, {String? confirmPassword}) {
+    if (value == null) {
+      return null;
+    }
+    if (confirmPassword != null && value != confirmPassword) {
+      return "Password not match";
+    }
+    if (value.isEmpty) {
+      return 'Please enter password';
+    }
+    return null;
+  }
+
+  static String? restrictedPassword(
+    String? value, {
+    String? confirmPassword,
+  }) {
+    if (value == null) {
+      return null;
+    }
+    if (value.isEmpty) {
+      return "password can not be empty";
+    }
+    if (confirmPassword != null && value != confirmPassword) {
+      return "Password not match";
+    }
+    final pattern = RegExp(
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$',
+    ).hasMatch(value);
+    if (pattern) {
       return null;
     } else {
-      return validator;
+      return "Your password must contain at least one capital, one small case letter, one number and 8 characters";
     }
+  }
+
+  static String? creditCardNumber(String? value) {
+    if (value == null) return null;
+    if (value.isEmpty) return "Please enter credit card number";
+    if (value.length < 16) return "Please enter with number minimum 16";
+    return null;
+  }
+
+  static String? cVV(String? value) {
+    if (value == null) return null;
+    if (value.isEmpty) return "Please enter cvv";
+    if (value.length < 3) return "Please enter with number minimum 3";
+    if (value.length > 3) return "Please enter with number maximum 3";
+    return null;
   }
 }
--- test/validator_test.dart
+++ test/validator_test.dart
@@ -1,6 +1,353 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:validator/validator.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  group(
+    "Validate name",
+    () {
+      test(
+        "return error message if the name is empty",
+        () {
+          final result = Validator.name("");
+          expect(result, "Please Enter Name");
+        },
+      );
+
+      test(
+        "return error message if name entered with less than 3 characters",
+        () {
+          final result = Validator.name("ab");
+          expect(result, "Name must be at least 3 characters");
+        },
+      );
+
+      test(
+        "return nothing aka:null if name entered with more than 3 characters",
+        () {
+          final result = Validator.name("abc");
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return nothing aka:null if name did not entered yet",
+        () {
+          final result = Validator.name(null);
+          expect(result, null);
+        },
+      );
+    },
+  );
+
+  group(
+    "Validate email",
+    () {
+      test(
+        "return error message if the email is empty",
+        () {
+          final result = Validator.email("");
+          expect(result, "Please enter email");
+        },
+      );
+
+      test(
+        "return error message if the email is incorrect",
+        () {
+          final result = Validator.email("abc");
+          expect(result, "Enter Valid Email");
+        },
+      );
+
+      test(
+        "return nothing aka:null if the email is entered and correct",
+        () {
+          final result = Validator.email("abc@gmail.com");
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return nothing aka:null if the email did not entered yet",
+        () {
+          final result = Validator.email(null);
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return custom message if the email entered is incorrect and we did provide a custom message",
+        () {
+          final result = Validator.email("abc", customMessage: "abc");
+          expect(result, "abc");
+        },
+      );
+
+      test(
+        "return nothing aka:null if the email is entered and correct even if we entered a custom message error",
+        () {
+          final result = Validator.email("abc@gmail.com", customMessage: "abc");
+          expect(result, null);
+        },
+      );
+    },
+  );
+
+  group(
+    "Validate country",
+    () {
+      test(
+        "return error message if the country is empty",
+        () {
+          final result = Validator.selectCountry("");
+          expect(result, "Please select country");
+        },
+      );
+
+      test(
+        "return nothing aka:null if the country is entered",
+        () {
+          final result = Validator.selectCountry("abc");
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return nothing aka:null if the country did not entered yet",
+        () {
+          final result = Validator.selectCountry(null);
+          expect(result, null);
+        },
+      );
+    },
+  );
+
+  group(
+    "Validate phone number",
+    () {
+      test(
+        "return error message if the phone number is empty",
+        () {
+          final result = Validator.phoneNumber("");
+          expect(result, "Please enter a phone number");
+        },
+      );
+
+      test(
+        "return error message if the phone number is less than 10 characters",
+        () {
+          final result = Validator.phoneNumber("123456789");
+          expect(result, "Please enter with number minimum 10");
+        },
+      );
+
+      test(
+        "return error message if the phone number is not start with 0",
+        () {
+          final result = Validator.phoneNumber("12345678901");
+          expect(result, "Please enter a valid phone number");
+        },
+      );
+
+      test(
+        "return error message if the phone number is not correct",
+        () {
+          final result = Validator.phoneNumber("019h3456789");
+          expect(result, "Please enter a valid phone number");
+        },
+      );
+
+      test(
+        "return nothing aka:null if the phone number is entered and correct",
+        () {
+          final result = Validator.phoneNumber("01934567890");
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return nothing aka:null if the phone number did not entered yet",
+        () {
+          final result = Validator.phoneNumber(null);
+          expect(result, null);
+        },
+      );
+    },
+  );
+
+  group(
+    "Validate Password",
+    () {
+      test(
+        "return null if password did not entered yet",
+        () {
+          final result = Validator.password(null);
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return error message if password is empty",
+        () {
+          final result = Validator.password("");
+          expect(result, "Please enter password");
+        },
+      );
+
+      test(
+        "return null if the password is correct",
+        () {
+          final result = Validator.password("123456");
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return null if the password entered and equal to confirm password",
+        () {
+          final result = Validator.password("abs", confirmPassword: "abs");
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return error message if the password entered and not equal to confirm password",
+        () {
+          final result = Validator.password("abs", confirmPassword: "abcd");
+          expect(result, "Password not match");
+        },
+      );
+    },
+  );
+
+  group(
+    "validate password is strong",
+    () {
+      test(
+        "return null if password did not entered yet",
+        () {
+          final result = Validator.restrictedPassword(null);
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return error message if password is empty",
+        () {
+          final result = Validator.restrictedPassword("");
+          expect(result, "password can not be empty");
+        },
+      );
+
+      test(
+        "return error in confirm password if it does not equal to password",
+        () {
+          final result = Validator.restrictedPassword(
+            "abs",
+            confirmPassword: "abcd",
+          );
+          expect(result, "Password not match");
+        },
+      );
+
+      test(
+        "return error if the password is week",
+        () {
+          String error =
+              "Your password must contain at least one capital, one small case letter, one number and 8 characters";
+          final result = Validator.restrictedPassword("1234567");
+          expect(result, error);
+        },
+      );
+
+      test(
+        "return null if the password is strong",
+        () {
+          final result = Validator.restrictedPassword("1234567asAS");
+          expect(result, null);
+        },
+      );
+    },
+  );
+
+  group(
+    "validate credit card number",
+    () {
+      test(
+        "return null if credit card number did not entered yet",
+        () {
+          final result = Validator.creditCardNumber(null);
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return error message if credit card number is empty",
+        () {
+          final result = Validator.creditCardNumber("");
+          expect(result, "Please enter credit card number");
+        },
+      );
+
+      test(
+        "return error message if credit card number is less than 16 characters",
+        () {
+          final result = Validator.creditCardNumber("123456789012345");
+          expect(result, "Please enter with number minimum 16");
+        },
+      );
+
+      test(
+        "return nothing aka:null if the credit card number is entered and correct",
+        () {
+          final result = Validator.creditCardNumber("1234567890123456");
+          expect(result, null);
+        },
+      );
+    },
+  );
+
+  group(
+    "validate cvv",
+    () {
+      test(
+        "return null if cvv did not entered yet",
+        () {
+          final result = Validator.cVV(null);
+          expect(result, null);
+        },
+      );
+
+      test(
+        "return error message if cvv is empty",
+        () {
+          final result = Validator.cVV("");
+          expect(result, "Please enter cvv");
+        },
+      );
+
+      test(
+        "return error message if cvv is less than 3 characters",
+        () {
+          final result = Validator.cVV("12");
+          expect(result, "Please enter with number minimum 3");
+        },
+      );
+
+      test(
+        "return error message if cvv is more than 3 characters",
+        () {
+          final result = Validator.cVV("12345");
+          expect(result, "Please enter with number maximum 3");
+        },
+      );
+
+      test(
+        "return nothing aka:null if the cvv is entered and correct",
+        () {
+          final result = Validator.cVV("123");
+          expect(result, null);
+        },
+      );
+    },
+  );
 }
